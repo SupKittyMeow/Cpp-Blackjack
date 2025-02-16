a5ef30e9d032177f152278a9c9f0930bdf8cfef9
supkittymeow <24541934-supkittymeow@users.noreply.replit.com>
2025-02-16 01:37:55 +0000

    User checkpoint: Refactor Card class to remove member function definitions from header file.

--- Card.cpp
+++ Card.cpp
@@ -1,16 +1,19 @@
-
 #include "Card.h"
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-string Card::getSuit() { return this->suit; }
-string Card::getRank() { return this->rank; }
-int Card::getValue() { return this->value; }
+string suit;
+string rank;
+int value;
 
-Card::Card(string s, string r, int v) {
+string getSuit() { return suit; }
+string getRank() { return rank; }
+int getValue() { return value; }
+
+Card(string s, string r, int v) {
   suit = s;
   rank = r;
   value = v;
-}
+};
